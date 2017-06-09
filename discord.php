c0659b4b729060c471e8ec01ebd0bb4776bf62db
Maxime <deepgamestm@gmail.com>
2017-06-09 17:17:56 +0200

    test

--- discord.php
+++ discord.php
@@ -4,7 +4,7 @@
     <head>
     </head>
      <body>
-     <iframe src="https://discordapp.com/widget?id=227108332480102400&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+         <p>GIT</p>
                  
     </body>
 </html>
